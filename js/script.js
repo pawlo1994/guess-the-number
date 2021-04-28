536d2202175b6f4371b560eabdd577c850808d1f
pawlo1994 <pawlo4-94@o2.pl>
2021-04-28 17:29:24 +0200

    some changes in functions added

--- js/script.js
+++ js/script.js
@@ -1,19 +1,12 @@
 {
-    const checkButton = document.querySelector(".js-checkButton");
-    const resetButton = document.querySelector(".js-resetButton");
-    const chooseLevelButton = document.querySelector(".js-chooseLevelButton");
     const minNumber = document.querySelector(".js-minNumber");
     const maxNumber = document.querySelector(".js-maxNumber");
     const userNumber = document.querySelector(".js-userNumber");
     const chancesLeft = document.querySelector(".js-chancesLeft");
     const result = document.querySelector(".js-result");
-    const gameForm = document.querySelector(".js-gameForm");
-    const levelForm = document.querySelector(".js-levelForm");
-    const gameSection = document.querySelector(".js-gameSection");
-    const chooseLevelSection = document.querySelector(".js-chooseLevelSection");
-    const formSectionHeader = document.querySelector(".js-formSectionHeader");
     const resultParagraph = document.querySelector(".js-resultParagraph");
     const range = document.querySelector(".js-range");
+    const formSectionHeader = document.querySelector(".js-formSectionHeader");
 
     let maxNumberValue = 0;
     let chosenNumber = 0;
@@ -37,10 +30,11 @@
         document.querySelector(".js-chancesLeft").innerText = chancesLeft.innerText;
     };
 
-    const swapButton = () => {
+    const swapButton = (reset, chooseLevel) => {
+        const checkButton = document.querySelector(".js-checkButton");
         checkButton.classList.toggle("section__button--hidden");
-        resetButton.classList.toggle("section__button--hidden");
-        chooseLevelButton.classList.toggle("section__button--hidden");
+        reset.classList.toggle("section__button--hidden");
+        chooseLevel.classList.toggle("section__button--hidden");
     };
 
     const chooseNumber = (max) => {
@@ -53,6 +47,8 @@
     };
 
     const swapSections = () => {
+        const gameSection = document.querySelector(".js-gameSection");
+        const chooseLevelSection = document.querySelector(".js-chooseLevelSection");
         chooseLevelSection.classList.toggle("section--hidden");
         gameSection.classList.toggle("section--hidden");
     };
@@ -112,10 +108,10 @@
         userNumber.disabled = true;
     };
 
-    const showWinOrLose = () => {
+    const showWinOrLose = (reset, chooseLevel) => {
         if ((+userNumber.value) === (+chosenNumber)) {
             showResult("you win!!!", result, resultParagraph, "section__paragraph--resultGood");
-            swapButton();
+            swapButton(reset, chooseLevel);
             formSectionHeader.classList.toggle("section__header--hidden");
         }
         else if ((chancesLeftValue === 0) || (+userNumber.min === +userNumber.max)) {
@@ -124,12 +120,12 @@
                 showResult(`Difference between min and max number equals 0!
                 Game interrupted.
                 Correct number is ${+chosenNumber}.`, result, resultParagraph, "section__paragraph--resultBad");
-                swapButton();
+                swapButton(reset, chooseLevel);
                 formSectionHeader.classList.toggle("section__header--hidden");
             } else {
                 showResult(`you lose!!!
                 Correct number is ${+chosenNumber}.`, result, resultParagraph, "section__paragraph--resultBad");
-                swapButton();
+                swapButton(reset, chooseLevel);
                 formSectionHeader.classList.toggle("section__header--hidden");
             };
         };
@@ -145,33 +141,41 @@
         userNumber.disabled = false;
     };
 
-    levelForm.addEventListener("submit", (event) => {
-        event.preventDefault();
-        chooseLevel();
-        setFocusOnUserNumber();
-    });
+    const init = () => {
+        const resetButton = document.querySelector(".js-resetButton");
+        const chooseLevelButton = document.querySelector(".js-chooseLevelButton");
+        const gameForm = document.querySelector(".js-gameForm");
+        const levelForm = document.querySelector(".js-levelForm");
 
-    gameForm.addEventListener("submit", (event) => {
-        event.preventDefault();
-        setFocusOnUserNumber();
-        checkNumber();
-        showWinOrLose();
-    });
+        levelForm.addEventListener("submit", (event) => {
+            event.preventDefault();
+            chooseLevel();
+            setFocusOnUserNumber();
+        });
 
-    resetButton.addEventListener("click", () => {
-        chooseNumber(maxNumberValue);
-        assignChancesValue(10);
-        hideResult();
-        swapButton();
-        formSectionHeader.classList.toggle("section__header--hidden");
-        setFocusOnUserNumber();
-    });
+        gameForm.addEventListener("submit", (event) => {
+            event.preventDefault();
+            setFocusOnUserNumber();
+            checkNumber();
+            showWinOrLose(resetButton, chooseLevelButton);
+        });
 
-    chooseLevelButton.addEventListener("click", () => {
-        assignChancesValue(10);
-        swapSections();
-        hideResult();
-        swapButton();
-        formSectionHeader.classList.toggle("section__header--hidden");
-    });
+        resetButton.addEventListener("click", () => {
+            chooseNumber(maxNumberValue);
+            assignChancesValue(10);
+            hideResult();
+            swapButton(resetButton, chooseLevelButton);
+            formSectionHeader.classList.toggle("section__header--hidden");
+            setFocusOnUserNumber();
+        });
+
+        chooseLevelButton.addEventListener("click", () => {
+            assignChancesValue(10);
+            swapSections();
+            hideResult();
+            swapButton(resetButton, chooseLevelButton);
+            formSectionHeader.classList.toggle("section__header--hidden");
+        });
+    };
+    init();
 }
